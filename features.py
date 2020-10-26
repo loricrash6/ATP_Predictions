acf504a0ca9dce9f9a855aabe35cfc607f908abc
Lorenzo Cascioli <44396386+loricrash6@users.noreply.github.com>
2020-10-26 12:40:53 +0100

    Common opponents + time discounting approaches

--- features.py
+++ features.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
 	CHECK: tourney round implementation (+ NOTE: RR matches are counted as the same round, so they cannot be used for other RR matches of the ame tourney)
 
 	TO DO: how to treat surface? Weight on other matches? Simple categorical feature? Consider only matches of the same surface (greatly reduces training data though..)
-	TO DO: fatigue modeling (num. games  played from start of tournament/last two weeks?)
+	TO DO: fatigue modeling (num. games played from start of tournament/last two weeks?)
 
 	TO DO: scale normal features to unit variance
 	TO DO: think of how to treat non-normal features
